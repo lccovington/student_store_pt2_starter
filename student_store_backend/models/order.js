7b70ba15821c59b51ae235931bc8ee5b39aed6a1
Lacie Covington <lacie.covington@gmail.com>
2022-07-05 17:21:18 -0400

    queries for step 5

--- student_store_backend/models/order.js
+++ student_store_backend/models/order.js
@@ -2,15 +2,42 @@ const db = require("../db")
 
 class Order {
 
-    static async listOrdersForUser() {
+    static async listOrdersForUser(user) {
+        
+        const result = await db.query(`
+        SELECT o.id AS "orderId",
+            o.customer_id AS "customerId",
+            od.quantity AS "quantity",
+            p.name AS "name",
+            p.price AS "price" FROM orders AS o
+        INNER JOIN order_details AS od ON o.id = od.order_id
+        INNER JOIN products as p ON p.id = od.product_id
+        WHERE o.customer_id = (SELECT id FROM users WHERE email = $1)
+    `, [user.email])
 
-        // return all orders that 
-        // the authenticated user has created
     }
 
-    static async createOrder() {
-        // will take a user's order and 
-        // store it in the database
+    static async createOrder({ order, user }) {
+        const user_id = await db.query(`SELECT id FROM users WHERE email = $1`, [user.email])
+        const query = `INSERT INTO orders (
+            customer_id
+        )
+        VALUES ($1)
+        RETURNING id;
+    `
+    const orderId = await db.query(query, [user_id]);
+    order.forEach((product) => {
+        const query = `INSERT INTO order_details (
+            order_id,
+            product_id,
+            quantity,
+            discount
+        )
+        VALUES ($1, $2, $3, $4)
+        RETURNING order_id;
+        `;
+        db.query(query, [orderId, product.id, product.quantity, product.discount])
+    })
     }
 }
 
